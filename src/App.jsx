ff3e3321993f12531fe0b9ae339a6bc275c514cc
Jang-DongHo <donghoo18@naver.com>
2025-11-22 17:43:46 +0900

    🐛 fix: 불필요한 about 페이지 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import './App.css'
 import Layout from './components/Layout'
-import About from './pages/About'
 import Blog from './pages/Blog'
 import BlogPost from './pages/BlogPost'
 import Home from './pages/Home'
@@ -15,7 +14,6 @@ function App() {
           <Route index element={<Home />} />
           <Route path="blog" element={<Blog />} />
           <Route path="blog/:id" element={<BlogPost />} />
-          <Route path="about" element={<About />} />
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
